b807afd97b046eb7b47f84088d482a4f084c9a85
Raviteja650 <ravitejab650@gmail.com>
2024-05-14 22:13:11 +0530

    Version codes files are changed and gitignore files has added

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
 console.log("index page");
 console.log("Version 2 updated");
-
-console.log("version 3 added");
--- src/config.js
+++ src/config.js
@@ -1,4 +1,2 @@
 console.log("config file");
 console.log("Version 2 updated");
-
-console.log("version 3 added");
